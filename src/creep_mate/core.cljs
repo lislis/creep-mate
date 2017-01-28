4108d53c6a5bf69a23bf1a3150f27c422f76b636
Igor Wiedler <igor@igor.io>
2017-01-28 13:08:11 +0100

    blue pixel is moving

--- src/creep_mate/core.cljs
+++ src/creep_mate/core.cljs
@@ -1,8 +1,18 @@
 (ns creep-mate.core
-  (:require [play-cljs.core :as p]))
+  (:require [play-cljs.core :as p]
+            [goog.events :as events]))
 
-(defonce game (p/create-game 500 500))
-(defonce state (atom {}))
+(defonce game (p/create-game 800 600))
+(defonce state (atom {:x 0, :y 0}))
+
+(def speed 10)
+
+(defn move [direction]
+  (case direction
+    :left (swap! state assoc :x (- (:x @state) speed))
+    :right (swap! state assoc :x (+ (:x @state) speed))
+    :up (swap! state assoc :y (- (:y @state) speed))
+    :down (swap! state assoc :y (+ (:y @state) speed))))
 
 (def main-screen
   (reify p/Screen
@@ -12,11 +22,19 @@
     (on-render [this]
       (p/render game
         [[:fill {:color "lightblue"}
-          [:rect {:x 0 :y 0 :width 500 :height 500}]]
-         [:fill {:color "black"}
-          [:text {:value "Hello, world!" :x (:text-x @state) :y (:text-y @state) :size 16 :font "Georgia" :style :italic}]]])
+          [:rect {:x (:x @state) :y (:y @state) :width 20 :height 20}]]])
       (swap! state update :text-x inc))))
 
 (doto game
   (p/start)
   (p/set-screen main-screen))
+
+  (events/listen js/window "keydown"
+                 (fn [^js/KeyboardEvent event]
+                   (let [key (.-keyCode event)]
+                     (case key
+                       87 (move :up)     ; w
+                       65 (move :left)   ; a
+                       83 (move :down)   ; s
+                       68 (move :right)  ; d
+                       false))))
