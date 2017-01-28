43d251584c7072661ab9eb2c546aa934ad2d0821
Igor Wiedler <igor@igor.io>
2017-01-28 13:37:50 +0100

    center-relative movement

--- src/creep_mate/core.cljs
+++ src/creep_mate/core.cljs
@@ -2,10 +2,13 @@
   (:require [play-cljs.core :as p]
             [goog.events :as events]))
 
-(defonce game (p/create-game 800 600))
-(defonce state (atom {:x 0, :y 0}))
+(def speed 1)
+(def player-size 20)
+(def screen-x 800)
+(def screen-y 600)
 
-(def speed 10)
+(defonce game (p/create-game screen-x screen-y))
+(defonce state (atom {:x 0, :y 0}))
 
 (defn move [direction]
   (case direction
@@ -21,15 +24,23 @@
     (on-hide [this])
     (on-render [this]
       (p/render game
-        [[:fill {:color "lightblue"}
-          [:rect {:x (:x @state) :y (:y @state) :width 20 :height 20}]]])
-      (swap! state update :text-x inc))))
+        (let [rendered-x (- (/ screen-x 2) (/ player-size 2))
+              rendered-y (- (/ screen-y 2) (/ player-size 2))]
+          [[:fill {:color "#ddd"}
+            [:rect {:x 0 :y 0 :width screen-x :height screen-y}]]
+           [:fill {:color "lightblue"}
+            [:rect {:x rendered-x :y rendered-y :width player-size :height player-size}]]
+           [:fill {:color "lightgreen"}
+            [:rect {:x (+ rendered-x (- (:x @state)) 100)
+                    :y (+ rendered-y (- (:y @state)) 100)
+                    :width 100
+                    :height 40}]]])))))
 
 (doto game
   (p/start)
   (p/set-screen main-screen))
 
-  (events/listen js/window "keydown"
+(events/listen js/window "keydown"
                  (fn [^js/KeyboardEvent event]
                    (let [key (.-keyCode event)]
                      (case key
