e03ecac903f6b77880c44f50c4909f7b8db65390
lislis <mail@lislis.de>
2017-01-29 10:22:10 +0100

    level sounds add images

---
+++ resources/untitled2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="roguelikeCity_magenta" tilewidth="16" tileheight="16" spacing="2" tilecount="910" columns="35">
+ <image source="../../../Downloads/Spritesheet/roguelikeCity_magenta.png" width="628" height="475"/>
+</tileset>
